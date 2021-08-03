145442042d1c99e133afec712f7b3ff6cb6fc628
abdallah <abdallah.s.alabd@gmail.com>
2021-08-03 06:09:56 +0300

    code chal 7

--- java/singlyLinkedLists/AppTest.java
+++ java/singlyLinkedLists/AppTest.java
@@ -1,7 +1,6 @@
 package singlyLinkedLists;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.*;
 
 public class AppTest {
 
@@ -9,6 +8,7 @@ public class AppTest {
     @Test
     public void testSinglyLinkedList() {
         SinglyLinkedLists list = new SinglyLinkedLists();
+
         assertNull("instantiate an empty linked list", list.head);
 
         SinglyLinkedLists list1 = new SinglyLinkedLists();
@@ -41,7 +41,7 @@ public class AppTest {
                 list4.includes(4));
 
         String str = "{ 1 } -> { 2 } -> { 3 } -> NULL";
-        assertEquals("return a collection of all the values that exist in the linked list", str, list4.toStr());
+        assertEquals("return a collection of all the values that exist in the linked list", str, list4.display());
     }
 
     // challenge 6
@@ -50,7 +50,7 @@ public class AppTest {
         SinglyLinkedLists list = new SinglyLinkedLists();
         list.append(1);
         String str = "{ 1 } -> NULL";
-        assertEquals("add a node to the end of the linked list", str, list.toStr());
+        assertEquals("add a node to the end of the linked list", str, list.display());
 
         ////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -58,7 +58,7 @@ public class AppTest {
         list.append(3);
         list.append(4);
         String str2 = "{ 1 } -> { 2 } -> { 3 } -> { 4 } -> NULL";
-        assertEquals("add multiple nodes to the end of a linked list", str2, list.toStr());
+        assertEquals("add multiple nodes to the end of a linked list", str2, list.display());
 
         ////////////////////////////////////////////////////////////////////////////////////////////////
         SinglyLinkedLists list1 = new SinglyLinkedLists();
@@ -67,13 +67,13 @@ public class AppTest {
         list1.append(4);
         list1.insertBefore(4, 3);
         String str3 = "{ 1 } -> { 2 } -> { 3 } -> { 4 } -> NULL";
-        assertEquals("insert a node before a node located i the middle of a linked list", str3, list1.toStr());
+        assertEquals("insert a node before a node located i the middle of a linked list", str3, list1.display());
 
         ////////////////////////////////////////////////////////////////////////////////////////////////
 
         list1.insertBefore(1, 0);
         String str4 = "{ 0 } -> { 1 } -> { 2 } -> { 3 } -> { 4 } -> NULL";
-        assertEquals("insert a node before the first node of a linked list", str4, list1.toStr());
+        assertEquals("insert a node before the first node of a linked list", str4, list1.display());
 
         ////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -83,12 +83,77 @@ public class AppTest {
         list2.append(4);
         list2.insertAfter(2, 3);
         String str5 = "{ 1 } -> { 2 } -> { 3 } -> { 4 } -> NULL";
-        assertEquals("insert after a node in the middle of the linked list", str5, list2.toStr());
+        assertEquals("insert after a node in the middle of the linked list", str5, list2.display());
 
         ////////////////////////////////////////////////////////////////////////////////////////////////
 
         list2.insertAfter(4, 5);
         String str6 = "{ 1 } -> { 2 } -> { 3 } -> { 4 } -> { 5 } -> NULL";
-        assertEquals("insert a node after the last node of the linked list", str6, list2.toStr());
+        assertEquals("insert a node after the last node of the linked list", str6, list2.display());
     }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    // challenge 7
+
+    @Test
+    public void testFindKFromEnd() {
+        SinglyLinkedLists list = new SinglyLinkedLists();
+        list.insert(9);
+        list.insert(8);
+        list.insert(7);
+        list.insert(6);
+        list.insert(5);
+        list.insert(4);
+        list.insert(3);
+        list.insert(2);
+        list.insert(1);
+        list.insert(0);
+        assertEquals("“Happy Path” where k is not at the end, but somewhere in the middle of the linked list",5,list.findKFromEnd(5));
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @Test
+    public void testFindKFromEnd1() {
+        SinglyLinkedLists list = new SinglyLinkedLists();
+        list.insert(1);
+        assertEquals("Where the linked list is of a size 1",1,list.findKFromEnd(1));
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @Test
+    public void testFindKFromEnd2() {
+        SinglyLinkedLists list = new SinglyLinkedLists();
+        list.insert(2);
+        list.insert(1);
+        list.insert(0);
+        assertEquals("Where k is greater than the length of the linked list","list Out Of Bounds",list.findKFromEnd(3));
+
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @Test
+    public void testFindKFromEnd3() {
+        SinglyLinkedLists list = new SinglyLinkedLists();
+        list.insert(0);
+        assertEquals("Where k and the length of the list are the same",0,list.findKFromEnd(0));
+
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @Test
+    public void testFindKFromEnd4() {
+        SinglyLinkedLists list = new SinglyLinkedLists();
+        list.insert(2);
+        list.insert(1);
+        list.insert(0);
+        assertEquals("Where k is not a positive integer","k is not a positive integer",list.findKFromEnd(-1));
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
 }
