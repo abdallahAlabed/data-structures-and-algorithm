f31e69012dbfd2af8be619762019341f763a0e79
abdallahAlabed <abdallah.s.alabd@gmail.com>
2021-08-09 02:42:39 +0300

    code challenge11

--- java/queue/app/src/main/java/queue/App.java
+++ java/queue/app/src/main/java/queue/App.java
@@ -1,8 +1,7 @@
 package queue;
 
 public class App {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
 
     }
 }
---
+++ java/queue/app/src/main/java/queue/PseudoQueue.java
@@ -0,0 +1,43 @@
+package queue;
+
+public class PseudoQueue <T> {
+    public Stack<T> firstStack;
+    public Stack<T> secondStack;
+
+    public PseudoQueue() {
+        this.firstStack = new Stack<>();
+        this.secondStack = new Stack<>();
+    }
+
+    public void enqueue(T value) {
+        firstStack.push(value);
+    }
+
+    public T dequeue() throws Exception {
+        while (firstStack.top != null){
+            secondStack.push(firstStack.pop());
+
+        }
+
+        T val = secondStack.pop();
+
+
+        while (secondStack.top != null){
+
+            firstStack.push(secondStack.pop());
+        }
+        return val;
+    }
+
+    @Override
+    public String toString() {
+        String str = "";
+        Node current = firstStack.top;
+        while (current != null){
+            str+="["+current.value+"]->";
+            current = current.next;
+        }
+        str+="NULL";
+        return str;
+    }
+}
