a8a06f06b0c1cfbda50689f9f740da5ddafbdaf8
abdallahAlabed <git config --global user.email>
2021-09-02 00:03:33 +0300

    not finished yet

--- java/QuickSort/app/src/main/java/QuickSort/QuickSort.java
+++ java/QuickSort/app/src/main/java/QuickSort/QuickSort.java
@@ -1,4 +1,41 @@
 package QuickSort;
 
+import java.util.Arrays;
+
 public class QuickSort {
+    public static int[] sort(int[] arr) {
+        quickSort(arr, 0, arr.length - 1);
+        return arr;
+    }
+
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+        for (int j = low; j <= high - 1; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return ++i;
+    }
+// i j pi
+    private static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivot = partition(arr, low, high);
+            quickSort(arr, low, pivot - 1);
+            quickSort(arr, pivot + 1, high);
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        System.out.print(Arrays.toString(arr));
+    }
 }
